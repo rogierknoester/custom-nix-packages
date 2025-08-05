659f11ca3c5692e75336c96edf346be9548b06a5
rogierknoester <dev@rogierknoester.nl>
2025-08-05 09:32:22 +0200

    update silverbullet

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
 
               src = final.fetchurl {
                 url = "https://github.com/silverbulletmd/silverbullet/releases/download/edge/silverbullet.js";
-                hash = "sha256-MY8ewq0rxjnazEZcAuc7RDc35Vee3W/2pVa4VQW9Kcs=";
+                hash = "sha256-KXisExTWWOZf16FfX3MYapmDZV362XZBc6K4aCQl8Nk=";
               };
             });
           };
--- packages/silverbullet.nix
+++ packages/silverbullet.nix
@@ -7,6 +7,6 @@ pkgs.silverbullet.overrideAttrs (oldAttrs: {
 
   src = fetchurl {
     url = "https://github.com/silverbulletmd/silverbullet/releases/download/edge/silverbullet.js";
-    hash = "sha256-MY8ewq0rxjnazEZcAuc7RDc35Vee3W/2pVa4VQW9Kcs=";
+    hash = "sha256-KXisExTWWOZf16FfX3MYapmDZV362XZBc6K4aCQl8Nk=";
   };
 })
