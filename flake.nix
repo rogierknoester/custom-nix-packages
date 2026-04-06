167c833e29a81f3f160f50e9cd109d387b124744
rogierknoester <dev@rogierknoester.nl>
2026-04-06 16:57:34 +0200

    remove override for silverbullet

--- flake.nix
+++ flake.nix
@@ -26,14 +26,6 @@
 
         flake = {
           overlays.default = final: prev: {
-            silverbullet = prev.silverbullet.overrideAttrs (oldAttrs: {
-              version = "edge";
-
-              src = final.fetchurl {
-                url = "https://github.com/silverbulletmd/silverbullet/releases/download/2.0.0/silverbullet.js";
-                hash = "sha256-O0QuzbY/ZdhOZvsUcgpZ55E+CSxbAsRxJmBQird5vCk=";
-              };
-            });
           };
         };
       };
